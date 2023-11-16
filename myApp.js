41b31065fa8b24cb94bd41db4266b06572558a22
Liliana-Hernandez-Velasco <122941293+Liliana-Hernandez-Velasco@users.noreply.github.com>
2023-11-16 01:21:20 +0000

    Push

--- myApp.js
+++ myApp.js
@@ -1,5 +1,68 @@
 let express = require('express');
 let app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/name", function(req, res) {
+  // Handle the data in the request
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
+app.use(function middleware(req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+app.get("/", (req,res)=> {
+  res.sendFile(__dirname + "/views/index.html");
+});
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+const mySecret = process.env['MESSAGE_STYLE']
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+  if (process.env['MESSAGE_STYLE'] === "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+
+});
+
+
+
+
+
 
 
 
